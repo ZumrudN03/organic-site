ef9fdb4882f28e8017c4c31359de2b14346f4b4d
Zumrud03 <nusretovazumrud@gmail.com>
2023-11-21 11:00:23 +0400

    new commit

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,3 +5,18 @@ const departmentList=document.querySelector(".navSearch .departments .content")
 departmentsBtn.addEventListener("click",function(){
     departmentList.classList.toggle("active")
 })
+
+
+
+$(document).ready(function(){
+    $('.cardSection').slick({
+    //   setting-name: setting-value
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: '<i class="fa-solid fa-angle-left"></i>',
+    nextArrow: '<i class="fa-solid fa-angle-right"></i>',
+    autoplay: true,
+    infinite: true,
+    autoplaySpeed: 1000,
+    });
+  });
